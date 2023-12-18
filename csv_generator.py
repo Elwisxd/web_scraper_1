c33b50908661436fb71d6f67c24e6d467c79a48f
Elvis <elvis.abolins@inbox.lv>
2023-12-18 20:11:07 +0200

    Minor fixes

--- csv_generator.py
+++ csv_generator.py
@@ -1,13 +1,14 @@
 import csv
 from scrapper import Scrapper
 from params import Params
+import os
 
 
 with open("data.csv", 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["No", "Name", "Manufacturer", "Price", "Price old"])
     s = Scrapper()
-    s.load_from_json(Params.get('folder_path')+Params.get('history_filename'))
+    s.load_from_json(os.path.join(Params.get('folder_path'),Params.get('history_filename')))
     index = 0
     for key, record in s.old_records.items():
         index += 1
